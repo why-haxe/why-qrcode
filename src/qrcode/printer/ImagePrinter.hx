71cb184d81110b49e9d8519e43ac8c3bfe1332af
Kevin Leung <kevinresol@gmail.com>
2017-08-31 14:41:04 +0800

    options

--- src/qrcode/printer/ImagePrinter.hx
+++ src/qrcode/printer/ImagePrinter.hx
@@ -4,38 +4,55 @@ import haxe.io.*;
 import qrcode.Printer;
 using tink.CoreApi;
 
-class ImagePrinter implements Printer<Noise> {
-	var bitmap:Bitmap
-	public function new(bitmap) {
-		this.bitmap = bitmap;
+private typedef Options = {
+	?intensityThreshold:Float,
+	?black:{r:Int, g:Int, b:Int, a:Int},
+	?white:{r:Int, g:Int, b:Int, a:Int},
+	?fillRatio:Float,
+	?canvas:{x:Int, y:Int, size:Int},
+}
+
+class ImagePrinter implements Printer<Bitmap> {
+	
+	var bitmap:Bitmap;
+	var intensityThreshold:Float;
+	var black:Pixel;
+	var white:Pixel;
+	var fillRatio:Float;
+	var canvas:{x:Int, y:Int, size:Int};
+	
+	public function new(bitmap:Bitmap, ?options:Options) {
+		this.bitmap = bitmap.clone();
+		
+		if(options == null) options = {};
+		intensityThreshold = options.intensityThreshold != null ? options.intensityThreshold : 0.25;
+		black = Pixel.make(options.black != null ? options.black : {r:0, g:0, b:0, a:160}, bitmap.format);
+		white = Pixel.make(options.white != null ? options.white : {r:255, g:255, b:255, a:160}, bitmap.format);
+		fillRatio = options.fillRatio != null ? options.fillRatio : 0.5;
+		canvas = options.canvas != null ? options.canvas : getDefaultCanvas(bitmap.width, bitmap.height);
 	}
 	
-	public function print(data:Data):Promise<Noise> {
-		var filename = 'letzbig';
-		var bitmap = readPng(sys.io.File.getBytes('bin/$filename.png'));
-		if(Math.abs(bitmap.width - bitmap.height) > 1) return new Error('Only accepts square images');
+	public function print(data:Data):Promise<Bitmap> {
+		var blockSize = Std.int(canvas.size / (data.size + 2)); // in pixel
+		var blockOffset = ((canvas.size - (data.size + 2) * blockSize) >> 1) + blockSize; // in pixel
+		var blockOffsetX = blockOffset + canvas.x;
+		var blockOffsetY = blockOffset + canvas.y;
 		
-		var blockSize = Std.int(bitmap.width / (data.size + 2)); // in pixel
-		var blockOffset = ((bitmap.width - (data.size + 2) * blockSize) >> 1) + blockSize; // in pixel
-		
-		var white = {r:255, g:255, b:255, a:160};
-		var black = {r:0, g:0, b:0, a:160};
-		
-		var fillOffset = Math.round(blockSize * 0.25);
-		var fillSize = blockSize - fillOffset * 2;
+		var fillSize = Math.round(blockSize * fillRatio);
+		var fillOffset = (blockSize - fillSize) >> 1;
 		
 		function blockAverageColor(x, y) {
-			return bitmap.average(x * blockSize + blockOffset, y * blockSize + blockOffset, blockSize, blockSize);
+			return bitmap.average(x * blockSize + blockOffsetX, y * blockSize + blockOffsetY, blockSize, blockSize);
 		}
 		
 		function fillBlock(x, y, offset, size, fill) {
-			var sx = x * blockSize + blockOffset + offset;
+			var sx = x * blockSize + blockOffsetX + offset;
 			var ex = sx + size;
-			var sy = y * blockSize + blockOffset + offset;
+			var sy = y * blockSize + blockOffsetY + offset;
 			var ey = sy + size;
 			for(i in sx...ex) for(j in sy...ey) {
 				var original = bitmap.getPixel(i, j);
-				var blended = original.blend(fill);
+				var blended = original.stamp(fill);
 				bitmap.setPixel(i, j, blended);
 			}
 		}
@@ -47,14 +64,13 @@ class ImagePrinter implements Printer<Noise> {
 				var isEye = data.isEye(x, y);
 				var offset = isEye ? 0 : fillOffset;
 				var size = isEye ? blockSize : fillSize;
+				
 				if(data.get(x, y)) {
-					if(intensity > 0.25) {
+					if(intensity > intensityThreshold)
 						fillBlock(x, y, offset, size, black);
-					}
 				} else {
-					if(intensity < 0.75) {
+					if(intensity < 1 - intensityThreshold)
 						fillBlock(x, y, offset, size, white);
-					}
 				}
 			}
 		}
@@ -77,35 +93,22 @@ class ImagePrinter implements Printer<Noise> {
 			fillBlock(-1, y, 0, blockSize, white);
 		}
 		
-		var bytes = writePng(bitmap);
-		sys.io.File.saveBytes('bin/$filename.bmp', bytes);
-		
-		return Noise;
+		return bitmap;
 	}
 	
-	function readPng(bytes:Bytes) {
-		var reader = new format.png.Reader(new BytesInput(bytes));
-		var data = reader.read();
-		var header = format.png.Tools.getHeader(data);
-		var pixels = format.png.Tools.extract32(data);
-		return new Bitmap(header.width, header.height, pixels, BGRA);
-	}
-	
-	function writePng(bitmap:Bitmap) {
-		var output = new BytesOutput();
-		var writer = new format.bmp.Writer(output);
-		var data = format.bmp.Tools.buildFromBGRA(bitmap.width, bitmap.height, bitmap.pixels);
-		writer.write(data);
-		return output.getBytes();
+	function getDefaultCanvas(w, h) {
+		if(w > h) return {x: (w - h) >> 1, y: 0, size: h};
+		if(w < h) return {x: 0, y: (h - w) >> 1, size: w};
+		return {x: 0, y: 0, size: w};
 	}
 }
 
 class Bitmap {
 	public var width(default, null):Int;
 	public var height(default, null):Int;
-	
+
 	public var pixels(default, null):Bytes;
-	var format(default, null):PixelFormat;
+	public var format(default, null):PixelFormat;
 	
 	public function new(width, height, pixels, format) {
 		this.width = width;
@@ -129,32 +132,35 @@ class Bitmap {
 		var r, g, b, a;
 		var offset = (y * width + x) * 4;
 		var data = pixels.getData();
-		switch format {
-			case BGRA:
-				b = Bytes.fastGet(data, offset + 0);
-				g = Bytes.fastGet(data, offset + 1);
-				r = Bytes.fastGet(data, offset + 2);
-				a = Bytes.fastGet(data, offset + 3);
-		}
 		
-		return {r:r, g:g, b:b, a:a};
+		return new Pixel(
+			Bytes.fastGet(data, offset + 0) << 24 |
+			Bytes.fastGet(data, offset + 1) << 16 |
+			Bytes.fastGet(data, offset + 2) << 8 |
+			Bytes.fastGet(data, offset + 3),
+			format
+		);
 	}
 	
 	public function average(x, y, w, h):Pixel {
-		var r = 0, g = 0, b = 0, a = 0;
+		var v0 = 0, v1 = 0, v2 = 0, v3 = 0;
 		var data = pixels.getData();
 		for(i in x...x+w) for(j in y...y+h) {
 			var offset = (j * width + i) * 4;
-			switch format {
-				case BGRA:
-					b += Bytes.fastGet(data, offset + 0);
-					g += Bytes.fastGet(data, offset + 1);
-					r += Bytes.fastGet(data, offset + 2);
-					a += Bytes.fastGet(data, offset + 3);
-			}
+			v0 += Bytes.fastGet(data, offset + 0);
+			v1 += Bytes.fastGet(data, offset + 1);
+			v2 += Bytes.fastGet(data, offset + 2);
+			v3 += Bytes.fastGet(data, offset + 3);
 		}
 		var n = w * h;
-		return {r:Math.round(r/n), g:Math.round(g/n), b:Math.round(b/n), a:Math.round(a/n)};
+		
+		return new Pixel(
+			Math.round(v0/n) << 24 |
+			Math.round(v1/n) << 16 |
+			Math.round(v2/n) << 8 |
+			Math.round(v3/n),
+			format
+		);
 	}
 	
 	public function clone() {
@@ -164,25 +170,68 @@ class Bitmap {
 	}
 }
 
-@:forward
-abstract Pixel({r:Int, g:Int, b:Int, a:Int}) from {r:Int, g:Int, b:Int, a:Int} {
-	public function intensity() 
-		return .3 * this.r / 255 + .59 * this.g / 255 + .11 * this.b / 255;
+@:structInit
+class Pixel {
+	public var r(get, never):Int;
+	public var g(get, never):Int;
+	public var b(get, never):Int;
+	public var a(get, never):Int;
 	
-	public function blend(other:Pixel):Pixel {
-		
+	var value:Int;
+	var format:PixelFormat;
+	
+	public static function make(v:{r:Int, g:Int, b:Int, a:Int}, format)
+		return switch format {
+			case BGRA: new Pixel(v.b<<24 | v.g<<16 | v.r<<8 | v.a, format);
+		}
+	
+	public function new(value, format) {
+		this.value = value;
+		this.format = format;
+	}
+	
+	public function intensity() {
+		return switch format {
+			case BGRA: .3 * r / 255 + .59 * g / 255 + .11 * b / 255;
+		}
+	}
+	
+	// stamp another pixel over this pixel
+	public function stamp(other:Pixel):Pixel {
 		var src_a = other.a / 255;
-		var dst_a = this.a / 255;
+		var dst_a = a / 255;
 		var one_minus_src_a = 1 - src_a;
 		var out_a = src_a + dst_a * one_minus_src_a;
 		
-		return {
+		var out = {
 			a: Math.round(out_a * 255),
-			r: Math.round((other.r * src_a + this.r * dst_a * one_minus_src_a) / out_a),
-			g: Math.round((other.g * src_a + this.g * dst_a * one_minus_src_a) / out_a),
-			b: Math.round((other.b * src_a + this.b * dst_a * one_minus_src_a) / out_a),
+			r: Math.round((other.r * src_a + r * dst_a * one_minus_src_a) / out_a),
+			g: Math.round((other.g * src_a + g * dst_a * one_minus_src_a) / out_a),
+			b: Math.round((other.b * src_a + b * dst_a * one_minus_src_a) / out_a),
 		}
+		
+		return Pixel.make(out, format);
 	}
+	
+	inline function get_r()
+		return switch format {
+			case BGRA: value >> 8 & 0xff;
+		}
+	
+	inline function get_g()
+		return switch format {
+			case BGRA: value >> 16 & 0xff;
+		}
+	
+	inline function get_b()
+		return switch format {
+			case BGRA: value >> 24 & 0xff;
+		}
+	
+	inline function get_a()
+		return switch format {
+			case BGRA: value & 0xff;
+		}
 }
 
 enum PixelFormat {
--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -1,17 +1,50 @@
 package ;
 
+import haxe.io.*;
+import qrcode.encoder.*;
+import qrcode.printer.ImagePrinter;
+import sys.io.File;
 using tink.CoreApi;
 
 class RunTests {
 
 	static function main() {
-		var encoder = new qrcode.encoder.NodeEncoder();
+		var encoder = new NodeEncoder();
+		var filename = 'colorful';
 		encoder.encode('http://links.letzbig.com/SEyV/3qmKWKZzGF')
-			.next(function(data) return new qrcode.printer.ImagePrinter().print(data))
+			.next(function(data) {
+				var bytes = File.getBytes('bin/$filename.png');
+				var bitmap = PngCodec.decode(bytes);
+				return new ImagePrinter(bitmap).print(data);
+			})
 			.handle(function(o) {
-				trace('\n',o.sure());
+				var bitmap = o.sure();
+				var bytes = BmpCodec.encode(bitmap);
+				File.saveBytes('bin/$filename.bmp', bytes);
 				travix.Logger.exit(0);
 			});
 	}
 
+}
+
+
+
+class PngCodec {
+	public static function decode(bytes:Bytes):Bitmap {
+		var reader = new format.png.Reader(new BytesInput(bytes));
+		var data = reader.read();
+		var header = format.png.Tools.getHeader(data);
+		var pixels = format.png.Tools.extract32(data);
+		return new Bitmap(header.width, header.height, pixels, BGRA);
+	}
+}
+
+class BmpCodec {
+	public static function encode(bitmap:Bitmap):Bytes {
+		var output = new BytesOutput();
+		var writer = new format.bmp.Writer(output);
+		var data = format.bmp.Tools.buildFromBGRA(bitmap.width, bitmap.height, bitmap.pixels);
+		writer.write(data);
+		return output.getBytes();
+	}
 }
