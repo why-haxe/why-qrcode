5c55dc8a92af536356d8ce9bcacc2aa4fcaa333d
Kevin Leung <kevinresol@gmail.com>
2019-02-26 13:05:45 +0800

    Support error correction level

--- src/qrcode/encoder/NodeEncoder.hx
+++ src/qrcode/encoder/NodeEncoder.hx
@@ -4,14 +4,25 @@ import qrcode.*;
 using tink.CoreApi;
 
 class NodeEncoder implements Encoder {
-	public function new() {}
+	var errorCorrectionLevel:ErrorCorrectionLevel;
+	public function new(?errorCorrectionLevel) {
+		this.errorCorrectionLevel = errorCorrectionLevel;
+	}
 	
 	public function encode(text:String):Promise<Data> {
-		return QrImage.matrix(text);
+		return QrImage.matrix(text, errorCorrectionLevel);
 	}
 }
 
 @:jsRequire('qr-image')
 extern class QrImage {
-	static function matrix(text:String):Array<Array<Int>>;
+	static function matrix(text:String, ?errorCorrectionLevel:ErrorCorrectionLevel):Array<Array<Int>>;
+}
+
+@:enum
+private abstract ErrorCorrectionLevel(String) {
+	var L = 'L';
+	var M = 'M';
+	var Q = 'Q';
+	var H = 'H';
 }
